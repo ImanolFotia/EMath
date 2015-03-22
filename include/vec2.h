b4ffac5a120e6a724dc99e2960fabc3a6354f29f
Psycho <psycho@psycho-eOS.(none)>
2015-03-22 00:21:18 -0300

    First Commit.

---
+++ include/vec2.h
@@ -0,0 +1,26 @@
+#ifndef EMATH_H_INCLUDED
+#define EMATH_H_INCLUDED
+
+class vec2{
+
+vec2(float, float);
+
+virtual ~vec2(){}
+
+public:
+
+float x, y;
+
+public:
+
+vec2 operator+(vec2);
+
+vec2 operator-(vec2);
+
+vec2 operator/(vec2);
+
+vec2 operator*(vec2);
+
+};
+
+#endif // EMATH_H_INCLUDED
